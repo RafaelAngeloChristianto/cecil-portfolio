dd2bcd80f09affef7edc5fbcccbc406782c0fb70
FarLeaf <144786286+RafaelAngeloChristianto@users.noreply.github.com>
2025-08-06 21:06:17 +0700

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer: React.FC = () => {
     return (
         <>
-            <div className="w-full bg-[#B91C1C] py-5">
+            <div className="w-full bg-[#B91C1C] py-5 absolute bottom-0">
                 <p className="text-center text-white">
                     &copy; 2025 Cecillia Tan Handoko. All rights reserved.
                 </p>
